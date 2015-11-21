fb8b396d39312045cf3c303579abfd10049b4fb2
Loren Barrick <lorenbarrick@gmail.com>
2015-11-21 13:57:50 -0800

    inital commit

---
+++ interview-practice.js
@@ -0,0 +1,62 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+               'o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Practice Problems:
+//-------------------
+// Use the higherOrderFunctions to solve the following problems:
+
+// Use filter to return just the vowels from the array letters
+filter(letters, function(letter) {
+  return ['a', 'e', 'i', 'o', 'u'].indexOf(letter) !== -1;
+});
+
+
+// Use each to print the odd numbers to the console
+each(numbers, function(number) {
+  if (number % 2 !== 0) {
+    console.log(number);
+  }
+});
+
+
+// Use reduce to return the sum of numbers
+reduce(numbers, function(sum, number) {
+  return sum + number;
+});
+
+
+// Use map to convert the array letters to all capital letters
+map(letters, function(letter) {
+  return letter.toUpperCase();
+});
+
+
+// Use filter to return every other letter in letters
+filter(letters, function(letter) {
+  return letters.indexOf(letter) % 2 !== 0;
+});
+
+
+// Modify the above function to return the other letters
+filter(letters, function(letter) {
+  return letters.indexOf(letter) % 2 === 0;
+});
+
+
+// Use filter to return the string "lmnop" from letters
+
+filter(letters, function(letter) {
+  return ['l', 'm', 'n', 'o', 'p'].indexOf(letter) !== -1;
+}).join(''); // HINT: more code here
+
+//use reduce and filter to add up all even numbers;
+reduce(filter(numbers, function(number) {
+  return number % 2 === 0;
+}), function(sum, number) {
+  return sum + number;
+});
+
+// Return the sum of the even numbers using filter and reduce together
+// NO HINTS: All of your code here

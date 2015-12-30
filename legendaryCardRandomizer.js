3751e5d7a87af4bba08d494ab461d559a556ddfe
Loren Barrick <lorenbarrick@gmail.com>
2015-12-29 16:20:14 -0800

    finished arrays

--- legendaryCardRandomizer.js
+++ legendaryCardRandomizer.js
@@ -5,3 +5,9 @@ var heroesArray = ['dominio', 'wolverine', 'colossus', 'forge', 'professor x', '
                   'hulk',];
 var mastermindArray = ['Apocalypse', 'mephisto', 'kingpin', 'stryfe', 'mr sinister', 'dr doom', 'loki', 'magento',
                       'red skull', ];
+var schemeArray = ['capture baby hope', 'detonate helicarrier', 'massive earthquake generator', 'organized crimewave',
+                  'Save Humanity', 'Steal Weaponized Plutonium', 'Transform citizens into demons', 'X-cutioners song',
+                  'The legacy Virus', 'Midtown Bank Robbery', 'Negative Zone Prison Breakout', 'Portals to the Dark Dimension',
+                  'Replace Earth\'s Leaders with Killbots', 'Secret Invasion of the Skrull Shapeshifters',
+                  'Super Hero Civil War', 'Unleash the Power of the Cosmic Cube'];
+                  
